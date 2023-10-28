3b8a62343a4398678cbf55807ca6c034d7a3c658
Brock <brockrbrown@gmail.com>
2023-10-28 17:29:21 -0400

    Added dragging, right click menu

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtCore import Qt, QTimer, QPoint
 from PyQt5.QtGui import QPixmap, QPainter, QContextMenuEvent
 from PyQt5.QtWidgets import QApplication, QWidget, QMenu
 from random import randint
@@ -9,6 +9,7 @@ class ImageCycleWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.oldPos = self.pos()
 
     def initUI(self):
         self.loading_animation = randint(1,2)
@@ -28,14 +29,12 @@ class ImageCycleWidget(QWidget):
 
     def nextImage(self):
         self.current_image_index = (self.current_image_index + 1) % len(self.image_paths)
-        print(self.current_image_index)
         self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
         pixmap = QPixmap(self.image_paths[self.current_image_index])
         painter.drawPixmap(self.rect(), pixmap)
-        
 
     def contextMenuEvent(self, event: QContextMenuEvent):
         contextMenu = QMenu(self)
@@ -44,6 +43,20 @@ class ImageCycleWidget(QWidget):
         if action == fooAction:
             print("foo!")
 
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.dragging = True
+            self.oldPos = event.globalPos()
+
+    def mouseMoveEvent(self, event):
+        if self.dragging:  # only move the widget if dragging is enabled
+            delta = QPoint (event.globalPos() - self.oldPos)
+            self.move(self.x() + delta.x(), self.y() + delta.y())
+            self.oldPos = event.globalPos()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.dragging = False
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
